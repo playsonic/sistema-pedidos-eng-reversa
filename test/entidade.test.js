de6839ecf750e2900124aa0e2638817f79b6dc83
Dan Mendes <danzitomendesnoguera@hotmail.com>
2026-04-27 16:29:11 -0300

    Modificando o código  de teste para ter o calculo total e o teste de descontos.

--- test/entidade.test.js
+++ test/entidade.test.js
@@ -1,4 +1,4 @@
-import { Produto } from '../src/models/entidade.js';
+import { Produto, Pedidos } from '../src/models/entidade.js';
 
 describe('Testes da classe Produto', () => {
 
@@ -21,6 +21,47 @@ describe('Testes da classe Produto', () => {
         expect(() => {
             Produto.criarProduto('pizzap', 'sabor_inexistente', 1);
         }).toThrow('Produto inválido ou não cadastrado no cardápio.');
+    });
+
+});
+
+describe('Testes da classe Pedidos', () => {
+
+    test('Cálculo do total: Deve somar os subtotais ao adicionar itens', () => {
+        const pedido = new Pedidos();
+
+        
+        const item1 = Produto.criarProduto('pizzap', 'calabresa', 2);
+
+        const item2 = Produto.criarProduto('suco', 'laranja', 1);
+
+        pedido.adicionarItem(item1);
+        expect(pedido.total).toBe(20);
+
+        pedido.adicionarItem(item2);
+        expect(pedido.total).toBe(23); 
+    });
+
+    test('Aplicação de desconto: Deve calcular o preço final com a estratégia fornecida', () => {
+        const pedido = new Pedidos();
+
+        const semDesconto = (total) => 0;
+        const desconto10 = (total) => total * 0.10;
+        const desconto20 = (total) => total * 0.20;
+
+        pedido.adicionarItem(Produto.criarProduto('pizzap', 'calabresa', 2)); // Total 20
+
+        expect(pedido.precoFinal(semDesconto)).toBe(21);
+
+        pedido.limpar();
+        pedido.adicionarItem(Produto.criarProduto('pizzam', 'calabresa', 5)); // Total 60
+
+        expect(pedido.precoFinal(desconto10)).toBe(57);
+
+        pedido.limpar();
+        pedido.adicionarItem(Produto.criarProduto('pizzag', 'calabresa', 10)); // Total 140
+
+        expect(pedido.precoFinal(desconto20)).toBe(119);
     });
 
 });
